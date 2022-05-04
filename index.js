9fae7a16b8f98e9937746d7a9c71e66bcfb32378
Simona <simona.smelhausova@gmail.com>
2022-05-04 20:05:35 +0200

    finalVerze

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let naTahu = 'circle';
 const hrac = document.querySelector('.tlacitko');
 
-const btnElm = document.querySelectorAll('button');
+const btnElm = document.querySelectorAll('.policka-container button');
 
 const hra = (event) => {
   if (event.target.classList.length == 0) {
@@ -22,4 +22,116 @@ const hra = (event) => {
 
 for (let i = 0; i < btnElm.length; i++) {
   btnElm[i].addEventListener('click', hra);
+}
+//ukol 5.
+const getSymbol = (field) => {
+  if (field.classList.contains('board__field--cross')) {
+    return 'cross';
+  } else if (field.classList.contains('board__field--circle')) {
+    return 'circle';
+  }
+};
+console.log(getSymbol(document.querySelector('.policka-container button')));
+
+const boardSize = 10;
+const fields = document.querySelectorAll('.policka-container button');
+const getField = (row, column) => {
+  return fields[row * boardSize + column];
+};
+
+console.log(getField(2, 5));
+
+const getPosition = (field) => {
+  let fieldIndex = 0;
+  while (fieldIndex < fields.length && field !== fields[fieldIndex]) {
+    fieldIndex++;
+  }
+
+  return {
+    row: Math.floor(fieldIndex / boardSize),
+    column: fieldIndex % boardSize,
+  };
+};
+
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+
+  let i;
+  let inRow = 1;
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
+  }
+
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+
+  let inColumn = 1;
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+
+  i = origin.row;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+
+  if (inColumn >= symbolsToWin) {
+    return true && setTimeout(popup, 500);
+  }
+
+  return false;
+};
+
+let tvojeHra = 'circle';
+const turn = document.querySelector('.kontrolky');
+const move = (event) => {
+  if (turn === 'circle') {
+    event.target.disabled = true;
+    event.target.classList.add('board__field--circle');
+    turn = 'cross';
+    turn.src = 'images/cross.svg';
+  } else {
+    event.target.disabled = true;
+    turn = 'circle';
+    event.target.classList.add('board__field--cross');
+    turn.src = 'images/circle.svg';
+  }
+  const winningPlayer = isWinningMove(event.target);
+  if (winningPlayer) {
+    const symbol = getSymbol(event.target);
+    if (confirm(`Hráč s ${symbol} vyhrál. Chcete hrát znovu?`) === true) {
+      location.reload();
+    }
+
+    setTimeout(function () {
+      if (confirm(`Hráč s ${symbol} vyhrál. Chcete hrát znovu?`) === true) {
+        location.reload();
+      }
+    }, 200);
+  }
+};
+
+const gameFieldBtn = document.querySelectorAll('.policka-container button');
+for (let i = 0; i < gameFieldBtn.length; i += 1) {
+  gameFieldBtn[i].addEventListener('click', move);
 }
